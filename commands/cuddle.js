92e03e07aa8fb02135cf68ee5b355deb17d35237
Master-mind4041 <ayushpanigrahi820@gmail.com>
2026-06-09 12:34:56 +0530

    Update cuddle.js

--- commands/cuddle.js
+++ commands/cuddle.js
@@ -10,9 +10,9 @@ module.exports = {
     }
 
     const gifs = [
-      "https://tenor.com/view/peach-loves-goma-peach-and-goma-peach-cat-goma-cat-peach-gif-6528988118635085144",
-      "https://tenor.com/view/snuggle-in-bed-mochi-peach-gif-1556884311883585909",
-      "https://tenor.com/view/milk-and-mocha-milk-mocha-milk-mocha-bear-milk-bear-mocha-bear-gif-7270252476476520589"
+      "https://cdn.discordapp.com/attachments/1467434180748120104/1513797800901476393/image2.gif?ex=6a290990&is=6a27b810&hm=4dcbbeb84cd4d91ae98b951ae73feb67bf911caa684e0cfe2c85192e853cc1a4&",
+      "https://cdn.discordapp.com/attachments/1467434180748120104/1513797800108888084/image0.gif?ex=6a290990&is=6a27b810&hm=31de716ed6db7dd2e6564229bec083778fee13d9467b56d51821f7a726c719b7&",
+      "https://cdn.discordapp.com/attachments/1467434180748120104/1513797800482177045/image1.gif?ex=6a290990&is=6a27b810&hm=5d712b5a44ed679a5accade3538fa417b285ad25615d80c0a2bf11af2c749a6d&"
     ];
 
     const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
@@ -20,7 +20,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor("#ff0000")
       .setAuthor({
-        name: `${message.author.username} cuddles ${user.username} closely!`,
+        name: `${message.member.displayName} cuddles ${user.username} closely!`,
         iconURL: message.author.displayAvatarURL()
       })
       .setImage(randomGif)
